f4faa43cb96a5dc90081e3cea7ff6e62987e211b
Hasan <rony.icpc@gmail.com>
2018-03-11 01:58:19 +0600

    initial commit

---
+++ app/src/main/java/com/heisenberg/barcodereader/BarcodeReaderApplication.java
@@ -0,0 +1,64 @@
+package com.heisenberg.barcodereader;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.multidex.MultiDex;
+
+/**
+ * Created by Hasan on 3/11/2018.
+ */
+
+public class BarcodeReaderApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
+
